fc76a6368d17c58c7f790b20f15871c679807dd8
Ersun Warncke <ewarncke@cpan.org>
2014-11-01 22:01:23 +0000

    Import of EWARNCKE/ArangoDB2-0.04 from CPAN.
    gitpan-cpan-distribution: ArangoDB2
    gitpan-cpan-version:      0.04
    gitpan-cpan-path:         EWARNCKE/ArangoDB2-0.04.tar.gz
    gitpan-cpan-author:       EWARNCKE
    gitpan-cpan-maturity:     released

---
+++ lib/ArangoDB2/Index.pm
@@ -0,0 +1,360 @@
+package ArangoDB2::Index;
+
+use strict;
+use warnings;
+
+use base qw(
+    ArangoDB2::Base
+);
+
+use Data::Dumper;
+use JSON::XS;
+
+# parameters that can be set when creating index or
+# are returned when creating/getting index
+our @PARAMS = qw(
+    byteSize constraint fields geoJson id ignoreNull
+    isNewlyCreated minLength size type unique
+);
+
+my $JSON = JSON::XS->new->utf8;
+
+
+
+# new
+#
+# create new instance.  optionally try to get index by name.
+sub new
+{
+    my($class) = shift;
+    # call inherited constructor
+    my $self = $class->SUPER::new(@_);
+    # if a name arg was passed then try to get
+    $self->get if $self->name;
+
+    return $self;
+}
+
+# byteSize
+#
+# get/set byteSize
+sub byteSize
+{
+    my($self, $byteSize) = @_;
+
+    if (defined $byteSize) {
+        $self->{byteSize} = $byteSize;
+        return $self;
+    }
+    else {
+        return $self->{byteSize};
+    }
+}
+
+# constraint
+#
+# get/set constraint value
+sub constraint
+{
+    my($self, $constraint) = @_;
+
+    if (defined $constraint) {
+        $self->{constraint} = $constraint ? JSON::XS::true : JSON::XS::false;
+        return $self;
+    }
+    else {
+        return $self->{constraint};
+    }
+}
+
+# create
+#
+# POST /_api/index
+sub create
+{
+    my($self, $index, $args) = @_;
+    # set default args
+    $index ||= {};
+    $args ||= {};
+    # require valid args
+    die 'Invalid Args'
+        unless ref $index eq 'HASH'
+        and ref $args eq 'HASH';
+    # set collection name
+    $args->{collection} = $self->collection->name;
+    # set args
+    for my $arg (@PARAMS) {
+        # if the arg was passed then set it
+        $self->$arg($args->{$args});
+        # use the set value if set
+        $args->{$arg} = $self->$arg
+            if defined $self->$arg;
+    }
+
+    my $res = $self->arango->http->post(
+        $self->api_path('index'),
+        $args,
+        $JSON->encode($index),
+    ) or return;
+
+    # get name from id
+    my($name) = $res->{id} =~ m{/(\d+)$}
+        or return;
+    $self->{name} = $name;
+    # copy properties from response
+    do { $self->{$_} = $res->{$_} if exists $res->{$_} } for @PARAMS;
+    # register
+    $self->collection->indexes->{$name} = $self;
+
+    return $self;
+}
+
+# delete
+#
+# DELETE /_api/index/{index-handle}
+sub delete
+{
+    my($self) = @_;
+
+    my $res = $self->arango->http->delete(
+        $self->api_path('index', $self->id),
+    ) or return;
+
+    # remove from register
+    delete $self->collection->indexes->{$self->name};
+
+    return $res;
+}
+
+# fields
+#
+# get/set fields
+sub fields
+{
+    my($self, $fields) = @_;
+
+    if (defined $fields) {
+        $self->{fields} = $fields;
+        return $self;
+    }
+    else {
+        return $self->{fields};
+    }
+}
+
+# geoJson
+#
+# get/set geoJson value
+sub geoJson
+{
+    my($self, $geoJson) = @_;
+
+    if (defined $geoJson) {
+        $self->{geoJson} = $geoJson ? JSON::XS::true : JSON::XS::false;
+        return $self;
+    }
+    else {
+        return $self->{geoJson};
+    }
+}
+
+# get
+#
+# GET /_api/index/{index-handle}
+sub get
+{
+    my($self, $name) = @_;
+    # name arg is optional.  If name is already set then
+    # object will morph into the new name.
+    $self->{name} = $name
+        if defined $name;
+
+    my $res = $self->arango->http->get(
+        $self->api_path('index', $self->id),
+    );
+
+    # copy properties from response
+    $self->{$_} = $res->{$_} for @PARAMS;
+
+    return $res;
+}
+
+# id
+#
+# get index id
+sub id
+{
+    my($self) = @_;
+
+    return defined $self->{id}
+        ? $self->{id}
+        : $self->name
+            ? join('/', $self->collection->name, $self->name)
+            : undef;
+}
+
+# ignoreNull
+#
+# get/set ignoreNull value
+sub ignoreNull
+{
+    my($self, $ignoreNull) = @_;
+
+    if (defined $ignoreNull) {
+        $self->{ignoreNull} = $ignoreNull ? JSON::XS::true : JSON::XS::false;
+        return $self;
+    }
+    else {
+        return $self->{ignoreNull};
+    }
+}
+
+# isNewlyCreated
+#
+# get isNewlyCreated value
+sub isNewlyCreated { $_[0]->{isNewlyCreated} }
+
+# list
+#
+# GET /_api/index
+sub list
+{
+    my($self, $args) = @_;
+    # set default args
+    $args ||= {};
+    # require valid args
+    die 'Invalid Args'
+        unless ref $args eq 'HASH';
+
+    $args->{collection} ||= $self->collection->name;
+
+    return $self->arango->http->get(
+        $self->api_path('index'),
+        $args
+    );
+}
+
+# minLength
+#
+# get/set minLength
+sub minLength
+{
+    my($self, $minLength) = @_;
+
+    if (defined $minLength) {
+        $self->{minLength} = $minLength;
+        return $self;
+    }
+    else {
+        return $self->{minLength};
+    }
+}
+
+# size
+#
+# get/set size
+sub size
+{
+    my($self, $size) = @_;
+
+    if (defined $size) {
+        $self->{size} = $size;
+        return $self;
+    }
+    else {
+        return $self->{size};
+    }
+}
+
+# type
+#
+# get/set type
+sub type
+{
+    my($self, $type) = @_;
+
+    if (defined $type) {
+        $self->{type} = $type;
+        return $self;
+    }
+    else {
+        return $self->{type};
+    }
+}
+
+# unique
+#
+# get/set unique value
+sub unique
+{
+    my($self, $unique) = @_;
+
+    if (defined $unique) {
+        $self->{unique} = $unique ? JSON::XS::true : JSON::XS::false;
+        return $self;
+    }
+    else {
+        return $self->{unique};
+    }
+}
+
+1;
+
+__END__
+
+=head1 NAME
+
+ArangoDB2::Index - ArangoDB2 index API methods
+
+=head1 METHODS
+
+=over 4
+
+=item new
+
+=item byteSize
+
+=item constraint
+
+=item create
+
+=item delete
+
+=item fields
+
+=item geoJson
+
+=item get
+
+=item id
+
+=item ignoreNull
+
+=item isNewlyCreated
+
+=item list
+
+=item minLength
+
+=item size
+
+=item type
+
+=item unique
+
+=back
+
+=head1 AUTHOR
+
+Ersun Warncke, C<< <ersun.warncke at outlook.com> >>
+
+http://ersun.warnckes.com
+
+=head1 COPYRIGHT
+
+Copyright (C) 2014 Ersun Warncke
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
---
+++ t/09-index.t
@@ -0,0 +1,101 @@
+use strict;
+use warnings;
+
+use Data::Dumper;
+use Test::More;
+
+use ArangoDB2;
+
+my $res;
+
+my $arango = ArangoDB2->new("http://localhost:8529");
+
+my $dbname = "ngukvderybvfgjutecbxzsfhyujmnvgf";
+my $database = $arango->database($dbname);
+my $collection = $database->collection('places');
+my $index = $collection->index();
+
+isa_ok($index, 'ArangoDB2::Index');
+
+# test required methods
+my @methods = qw(
+    new
+    byteSize
+    constraint
+    create
+    delete
+    fields
+    geoJson
+    get
+    id
+    ignoreNull
+    isNewlyCreated
+    list
+    minLength
+    size
+    type
+    unique
+);
+
+for my $method (@methods) {
+    can_ok($index, $method);
+}
+
+# skip tests against the actual ArangoDB2 server unless
+# LIVE_TEST env param is set
+if (!$ENV{LIVE_TEST}) {
+    diag("Skipping live API tests - set LIVE_TEST=1 to enable");
+    done_testing();
+    exit;
+}
+
+# create database
+$database->create();
+#create collection
+$res = $collection->create();
+
+# create some data
+$collection->document->create({
+    city => "Portland",
+    state => "Oregon",
+    latitude => 45.527,
+    longitude => -122.683,
+});
+$collection->document->create({
+    city => "Vancouver",
+    state => "Washington",
+    latitude => 45.637,
+    longitude => -122.670,
+});
+$collection->document->create({
+    city => "Beaverton",
+    state => "Oregon",
+    latitude => 45.488,
+    longitude => -122.813,
+});
+
+# create hash index
+$res = $index->create({
+    type => "hash",
+    fields => ["city", "state"],
+});
+ok($res, "create hash index");
+ok($index->name, "create: name");
+ok($index->data, "create: data");
+# check list
+$res = $index->list;
+ok($res->{indexes}, "list");
+ok( ( grep { $_->{id} eq $index->id } @{$res->{indexes}} ), "index created" );
+# delete index
+$index->delete;
+# check list
+$res = $index->list;
+ok( !( grep { $_->{id} eq $index->id } @{$res->{indexes}} ), "index deleted" );
+
+
+# delete
+$res = $collection->delete();
+# delete database
+$res = $arango->database($dbname)->delete();
+
+done_testing();
